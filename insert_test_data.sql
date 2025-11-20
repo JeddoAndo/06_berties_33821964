2029003470c0d1ae70d29dee47405ee49422c4f3
JeddoAndo <jedf6190@gmail.com>
2025-11-20 12:45:30 +0000

    Registered user 'gold' with password

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -3,3 +3,14 @@
 USE berties_books;
 
 INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
+
+# Insert default user for lab marking
+
+INSERT INTO users (username, first, last, email, hashedPassword)
+VALUES (
+    'gold',
+    'Gold',
+    'Smiths',
+    'gold@example.com',
+    '$2b$10$Rk5cbXP0pZSVm0PQUQ1ijdJFfxp4r9mM3i9FXE.NFrjIR3JghJCha'
+);
