0d218744837faba6e87492dd436b9667985bd77a
JeddoAndo <jedf6190@gmail.com>
2025-12-04 19:40:20 +0000

    Lab 9a Task 2 - Changed json output to be human-readable and added a link on homepage.

--- routes/weather.js
+++ routes/weather.js
@@ -14,7 +14,16 @@ router.get('/', function(req, res, next) {
         if(err){
             next(err)
         } else {
-            res.send(body)
+            // res.send(body);
+
+            var weather = JSON.parse(body);
+
+            var wmsg = "It is " + weather.main.temp +
+                " degrees in " + weather.name +
+                "! <br> The humidity now is: " +
+                weather.main.humidity;
+
+            res.send(wmsg);
         }
     });
 });
